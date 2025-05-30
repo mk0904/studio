c4ee1411f23dae06979b28510f244c41ac988cb4
Manish Kumar <mk10092004@gmail.com>
2025-05-30 19:47:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: The default export is not a React Component in "/bhr/analytics/page". Error source: Call Stack
    > 27
    >
    > Show 27 ignore-listed frame(s)

--- src/app/(dashboards)/bhr/analytics/page.tsx
+++ src/app/(dashboards)/bhr/analytics/page.tsx
@@ -1,3 +1,6 @@
 
 // This file is intentionally left blank or can be removed.
 // The analytics functionality has been moved to ZHR.
+export default function BHRAnalyticsPagePlaceholder() {
+  return null;
+}
