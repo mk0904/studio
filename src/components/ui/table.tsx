9f9657f7aff9b186329735467d2d3b5306a91adb
Manish Kumar <mk10092004@gmail.com>
2025-06-01 23:33:54 +0000

    remove the black lines, outlines... coming the outlines...and all

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -9,7 +9,7 @@ const Table = React.forwardRef<
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm border-0", className)} // Added border-0
       {...props}
     />
   </div>
